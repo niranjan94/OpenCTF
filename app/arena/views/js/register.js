01d5c6106d7f4f39c007f5754fd438091f8069d6
Niranjan Rajendran <niranjan94@yahoo.com>
2015-04-03 20:33:58 +0530

    Registration Page * added custom val function for username (no functionality yet) * started validations * added team page for registration

--- app/arena/views/js/register.js
+++ app/arena/views/js/register.js
@@ -2,9 +2,99 @@ $("#pjax-container").find("*").unbind();
 $('.ui.checkbox').checkbox();
 $('.dropdown').dropdown();
 $('.ui.radio.checkbox').checkbox();
-function openTos(){
-    $("#tos-content").html("");
-    $('.modal')
-        .modal('show')
-    ;
+function openTos(e){
+    /*$("#tos-content").html("");*/
+    $('.modal').modal('show');
 }
+
+$.fn.form.settings.rules.username = function(value) {
+    return true;
+};
+
+$('.ui.form.step-1')
+    .form({
+        gender: {
+            identifier  : 'gender',
+            rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please select your gender'
+                }
+            ]
+        },
+        firstName: {
+            identifier  : 'first-name',
+            rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter your first name'
+                }
+            ]
+        },
+        lastName: {
+            identifier  : 'last-name',
+            rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter your last name'
+                }
+            ]
+        },
+        about: {
+            identifier  : 'about',
+            rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Tell us about yourself'
+                }
+            ]
+        },
+        username: {
+            identifier  : 'username',
+            rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter desired username'
+                },
+                {
+                    type   : 'username',
+                    prompt : 'That username is already taken'
+                }
+            ]
+        },
+        password: {
+            identifier  : 'password',
+            rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter a password'
+                }
+            ]
+        },
+        email: {
+            identifier  : 'email',
+            rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter your email address'
+                },
+                {
+                    type   : 'email',
+                    prompt : 'A valid email address is required'
+                }
+            ]
+        },
+        terms: {
+            identifier : 'agree-tos',
+            rules: [
+                {
+                    type   : 'checked',
+                    prompt : 'You must agree to the terms and conditions'
+                }
+            ]
+        }
+    }, {
+        inline : true,
+        on     : 'change'
+    })
+;
