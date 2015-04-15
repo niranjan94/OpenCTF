db8cb83a52ffe01a834f45d693c5e2c4b08ecc62
Niranjan Rajendran <niranjan94@yahoo.com>
2015-04-15 15:28:51 +0530

    Composer package update.
    * Removed endroid/qrcode.
    * updated Carbon datetime library.

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,7 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Symfony\\Component\\Translation\\' => array($vendorDir . '/symfony/translation'),
     'Identicon' => array($vendorDir . '/yzalis/identicon/src'),
-    'Endroid' => array($vendorDir . '/endroid/qrcode/src'),
     'Carbon' => array($vendorDir . '/nesbot/carbon/src'),
 );
